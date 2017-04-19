5e8bb3bf66225a06444aa8cd292a5bf468450db2
vjuliangutierrez <vjulian.gutierrez@udea.edu.co>
2017-04-19 12:56:56 -0500

    Modal Registro Horarios
    Se refina el modal que permite el registro de horarios de los medicos.

--- src/components/Horarios.vue
+++ src/components/Horarios.vue
@@ -37,17 +37,67 @@
 
 
 
-      <a href="#modal1" class="btn btn-primary btn-lg" data-toggle="modal">Nuevo</a>
+      <button href="#modal1" class="btn btn-primary black-background white" data-toggle="modal">Nuevo</button>
 
 
       <div class="modal fade" id="modal1">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">
+                       <span aria-hidden="true">&times;</span>
+                       <span class="sr-only">Close</span>
+                </button>
+                <h4 class="modal-title">Ingresar Nuevo Horario</h4>
+
+              </div>
+
+              <div class="modal-body">
+                 <h5 class="text-left" >Ingrese la siguiente informacion referente al registro de un nuevo horario para un medico.</h5>
+                  <form role="form">
+                  <div class="form-group">
+                    <label for="entradaNombres">Nombres</label>
+                      <input type="nombres" class="form-control"
+                      id="entradaNombres" placeholder="Ingrese los Nombres"/>
+                  </div>
+                  <div class="form-group">
+                    <label for="entradaApellidos">Apellidos</label>
+                      <input type="apellidos" class="form-control"
+                          id="entradaApellidos" placeholder="Ingrese los Apellidos"/>
+                  </div>
+                  <div class="form-group">
+                    <label for="entradaEmail">Email</label>
+                      <input type="email" class="form-control"
+                          id="entradaEmail" placeholder="Ingrese el Email"/>
+                  </div>
+                  <!-- <div class="checkbox">
+                    <label>
+                        <input type="checkbox"/> Check me out
+                    </label>
+                  </div> -->
+                  <!-- <button type="submit" class="btn btn-default">Aceptar</button> -->
+                </form>
+                
 
 
-          
+              </div>
 
 
 
+              <div class="modal-footer">
+                
+                <button type="button" class="btn btn-default"
+                        data-dismiss="modal">
+                            Cerrar
+                </button>
+                <button type="button" class="btn btn-primary black-background white">
+                             Aceptar
+                </button>
 
+
+              </div>
+            </div>
+          </div>
      </div>
 
 
@@ -88,4 +138,11 @@ li {
 a {
   color: #42b983;
 }
+
+
+.black-background {background-color:#4d4d4d;}
+.white {color:#ffffff;}
+
+
+
 </style>
